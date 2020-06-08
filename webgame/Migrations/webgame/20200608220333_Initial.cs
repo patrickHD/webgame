68c874726ebc91d8b5239c958cdd1cda1fee6983
Patrick H Daniel Jr <patrickhdaniel@outlook.com>
2020-06-08 17:06:32 -0500

    use postgres

---
+++ webgame/Migrations/webgame/20200608220333_Initial.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace webgame.Migrations.webgame
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HighScore",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UserName = table.Column<string>(type: "text", nullable: true),
+                    ReleaseDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Score = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HighScore", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HighScore");
+        }
+    }
+}
--- webgame/Startup.cs
+++ webgame/Startup.cs
@@ -47,7 +47,7 @@ namespace webgame
             });
 
             services.AddDbContext<webgameContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("webgameContext")));
+                    options.UseNpgsql(Configuration.GetConnectionString("webgameContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
